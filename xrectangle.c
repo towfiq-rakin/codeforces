45b0895fdd09b8223694863b13c494027d92852c
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2024-11-26 00:17:16 +0600

    Update xrectangle.c

--- xrectangle.c
+++ xrectangle.c
@@ -7,25 +7,26 @@ int main(){
         printf("Enter an even number\n");
         return 1;
     }
-
+    // First row
     for(int i = 1; i<=n; i++){
         printf("@");
     }
     printf("\n");
+    
+    /* iterate over the 2nd to 2nd last row
+    as we have already printed the first and last row */
     for(int i = 2; i <= n-1; i++){
-        printf("@");
+        printf("@"); // to print @ at the start of the row
         for(int j = 2; j <= n-1; j++){
-            if(j == i || j == n-i+1){
+            if(j == i || j == (n-i)+1)
                 printf("@");
-            }
-            else{
+            else
                 printf(" ");
-            }
         }
-        printf("@");
+        printf("@"); // to print @ at the end of the row
         printf("\n");
     }
-
+    // Last row
     for(int i = 1; i<=n; i++){
         printf("@");
     }
